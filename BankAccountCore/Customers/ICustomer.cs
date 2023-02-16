def5975f682a5436933068f18c267a58ff86162f
ortizdavid <ortizaad1994@gmail.com>
2023-02-16 16:29:35 +0100

    Uploading Withdraw

--- BankAccountCore/Customers/ICustomer.cs
+++ BankAccountCore/Customers/ICustomer.cs
@@ -8,7 +8,7 @@ namespace BankAccount.Customers
 {
     public interface ICustomer
     {
-        /*public string Id { get; set; }
-        public string Name { get; set; }*/
+        public string Id { get; set; }
+        public string Name { get; set; }
     }
 }
